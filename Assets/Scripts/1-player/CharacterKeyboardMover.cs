434f44b5bfe87b428675fc03d69c04c0dc4d7a46
DESKTOP-HLUJT3C\yinon <yinonss@hotmail.com>
2020-12-22 16:07:31 +0200

    Add jump option

--- Assets/Scripts/1-player/CharacterKeyboardMover.cs
+++ Assets/Scripts/1-player/CharacterKeyboardMover.cs
@@ -10,6 +10,8 @@ public class CharacterKeyboardMover: MonoBehaviour {
     [Tooltip("Speed of player keyboard-movement, in meters/second")]
     [SerializeField] float _speed = 3.5f;
     [SerializeField] float _gravity = 9.81f;
+    [SerializeField] KeyCode jump_key;
+    readonly float jumpHight = 10;
 
     private CharacterController _cc;
     void Start() {
@@ -21,14 +23,26 @@ public class CharacterKeyboardMover: MonoBehaviour {
     void Update()  {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        float y = Input.GetAxis("Jump");
         //if (x == 0 && z == 0) return;
         velocity.x = x * _speed;
         velocity.z = z * _speed;
-        if (!_cc.isGrounded) {
-            velocity.y -= _gravity*Time.deltaTime;
+        if (!_cc.isGrounded)
+        {
+            velocity.y -= _gravity * Time.deltaTime;
         }
-        // Click Up: velocity = (0,0,1)
-        velocity = transform.TransformDirection(velocity);
+        else 
+        {
+            velocity.y = y * _speed;
+        }
+        if (Input.GetKeyDown(jump_key))
+        {
+            Debug.Log(_cc.isGrounded);
+            y += jumpHight;
+        }
+        
+            // Click Up: velocity = (0,0,1)
+            velocity = transform.TransformDirection(velocity);
         //Debug.Log("velocity="+velocity+" isGrounded="+ _cc.isGrounded);
         _cc.Move(velocity * Time.deltaTime);
     }
---
+++ Assets/Scripts/1-player/JumpControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpControl : MonoBehaviour
+{
+    [SerializeField] KeyCode jump_key;
+    const float z = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(jump_key))
+        {
+            this.transform.position = new Vector3(z, z, z);
+        }
+    }
+}
